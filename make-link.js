864f1080419cb61dfefaa8cae5381d15dc07ed32
Kristján Oddsson <koddsson@gmail.com>
2017-09-10 00:25:18 +0100

    to: add a make-link command

---
+++ make-link.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+const [,, title, link] = process.argv
+
+const usage = () => {
+  console.log('make-link [title] [url]')
+}
+
+if (!title || !link) {
+  usage()
+}
+
+fs.readFile('./to/_template', 'utf8', (error, data) => {
+  if (error) {
+    console.log(error)
+  }
+
+  const newData = data.replace('LINK_GOES_HERE', link);
+
+  fs.writeFile(`./to/${title}.html`, newData, error => {
+    if (error) {
+      console.log(error)
+    }
+  })
+})
