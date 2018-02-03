80198a91d89303596cef5a6f7e3d36bf4d86b3e7
Kristján Oddsson <koddsson@gmail.com>
2018-02-03 12:21:59 +0000

    eslint --fix make-links.js

--- make-link.js
+++ make-link.js
@@ -1,25 +1,25 @@
 const fs = require('fs');
 
-const [,, title, link] = process.argv
+const [,, title, link] = process.argv;
 
 const usage = () => {
-  console.log('make-link [title] [url]')
-}
+  console.log('make-link [title] [url]');
+};
 
 if (!title || !link) {
-  usage()
+  usage();
 }
 
 fs.readFile('./to/_template', 'utf8', (error, data) => {
   if (error) {
-    console.log(error)
+    console.log(error);
   }
 
   const newData = data.replace('LINK_GOES_HERE', link);
 
   fs.writeFile(`./to/${title}.html`, newData, error => {
     if (error) {
-      console.log(error)
+      console.log(error);
     }
-  })
-})
+  });
+});
