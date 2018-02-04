6bf282b8ae24df9dc8a29940c895e4bffff88a4b
Kristján Oddsson <koddsson@gmail.com>
2018-02-04 19:23:18 +0000

    move make-link scripts to `to/` folder

--- to/make-link.js
+++ to/make-link.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const [,, title, link] = process.argv;
+const [, , title, link] = process.argv;
 
 const usage = () => {
   console.log('make-link [title] [url]');
@@ -10,14 +10,14 @@ if (!title || !link) {
   usage();
 }
 
-fs.readFile('./to/_template', 'utf8', (error, data) => {
+fs.readFile('./_template', 'utf8', (error, data) => {
   if (error) {
     console.log(error);
   }
 
   const newData = data.replace('LINK_GOES_HERE', link);
 
-  fs.writeFile(`./to/${title}.html`, newData, error => {
+  fs.writeFile(`./${title}.html`, newData, error => {
     if (error) {
       console.log(error);
     }
