17b164c4c45bd05981040a5545939f41e3c37f07
Kristján Oddsson <koddsson@gmail.com>
2018-02-04 19:24:39 +0000

    lint recipes folder

--- recipes/validate.js
+++ recipes/validate.js
@@ -1,26 +1,26 @@
-const fs = require("fs");
-const path = require("path");
-const { promisify } = require("util");
-const Ajv = require("ajv");
-const chalk = require("chalk");
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const Ajv = require('ajv');
+const chalk = require('chalk');
 
 const readdir = promisify(fs.readdir);
 const readFile = promisify(fs.readFile);
 const ajv = new Ajv({ allErrors: true });
-const fullPath = path.resolve(__dirname + "/recipes/");
+const fullPath = path.resolve(__dirname + '/recipes/');
 
 let exitCode = 0;
 
 async function main() {
-  const schema = await readFile(path.resolve(__dirname + "/schema.json"));
-  const validate = ajv.compile(JSON.parse(schema.toString("utf8")));
+  const schema = await readFile(path.resolve(__dirname + '/schema.json'));
+  const validate = ajv.compile(JSON.parse(schema.toString('utf8')));
   const filenames = await readdir(fullPath);
 
   for (const filename of filenames) {
-    const fullFilename = path.resolve(__dirname + "/recipes/" + filename);
+    const fullFilename = path.resolve(__dirname + '/recipes/' + filename);
     const recipe = await readFile(fullFilename);
 
-    const valid = validate(JSON.parse(recipe.toString("utf8")));
+    const valid = validate(JSON.parse(recipe.toString('utf8')));
     if (!valid) {
       console.log(
         chalk.magenta.underline(`${filename} had the following errors:`)
@@ -28,7 +28,7 @@ async function main() {
       console.log(
         validate.errors
           .map(error => chalk.blue(` ${error.dataPath} ${error.message}`))
-          .join("\n")
+          .join('\n')
       );
       exitCode = 1;
     }
