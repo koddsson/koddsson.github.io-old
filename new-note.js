288ea86e1dec811bb2304285cbf1e877517521a9
Kristján Oddsson <koddsson@gmail.com>
2018-08-22 11:49:53 +0100

    improvements to note script

--- new-note.js
+++ new-note.js
@@ -1,6 +1,13 @@
 const fs = require("fs");
 const { fork, spawn } = require("child_process");
 
+const spawnSync = (cmd, args, options) => {
+  return new Promise(resolve => {
+    const process = spawn(cmd, args, options);
+    process.on("close", resolve);
+  });
+};
+
 const date = new Date();
 const baseFilename = `./notes/${date.toISOString().split("T")[0]}`;
 let filename = baseFilename;
@@ -18,5 +25,12 @@ pubDate: ${date.toISOString()}
 
 fs.writeFileSync(`${filename}.md`, template);
 
-spawn("nvim", [`${filename}.md`], { stdio: "inherit" });
-spawn("node", ["regenerate-notes.js"], { stdio: "inherit" });
+(async function() {
+  await spawnSync("git", ["stash"], { stdio: "inherit" });
+  await spawnSync("nvim", [`${filename}.md`], { stdio: "inherit" });
+  await spawnSync("node", ["regenerate-notes.js"], { stdio: "inherit" });
+  await spawnSync("git", ["add", "."], { stdio: "inherit" });
+  await spawnSync("git", ["commit", "-m", "🐦 chrip 🦗"], { stdio: "inherit" });
+  await spawnSync("git", ["stash", "pop"], { stdio: "inherit" });
+  await spawnSync("git", ["--no-pager", "show"], { stdio: "inherit" });
+})();
